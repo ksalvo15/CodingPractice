cd3221efbabc8734a526cc7e14c94cc5b7540035
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-11-16 11:02:07 -0800

    added practice

---
+++ C#/FindNeedle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+//prompt - find the needle in the array and make it say it appears at position 6
+
+namespace FindNeedle
+{
+    class Program
+    {
+        static void Main()
+        {            
+            FindNeedle();
+        }
+        static void FindNeedle()
+        {
+            Console.WriteLine("Hello World!");
+
+            List<string> needlelist = new List<string> { "hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk" };
+
+            int needleindex = needlelist.IndexOf("needle");
+
+            if (needleindex != -1)
+            {
+                Console.WriteLine("Found the needle at position {0}", needleindex +1);
+            }
+
+
+            else { 
+                Console.WriteLine("I need not find the needle"); 
+            }
+
+            
+        }
+    }
+}
